62d468a34157329f5e7da799ecb903195f3568b7
Devwarlt <devwarlt@loesoft.org>
2020-10-10 23:57:42 -0300

    Update user_controller.py

--- user_controller.py
+++ user_controller.py
@@ -1,7 +1,7 @@
 import _sqlite3
 
-conect = _sqlite3.connect('user.db')
-mycursor = conect.cursor()
+conn = _sqlite3.connect('user.db')
+cur = conn.cursor()
 
 
 class ControlUser:
@@ -19,28 +19,28 @@ Menu de opções, digite uma das opções:
         print('-=-' * 15)
 
     def register(self, nick, senha):
-        mycursor.execute('INSERT INTO login(nick, senha) VALUES(?, ?)', (nick, senha))
-        conect.commit()
+        cur.execute('INSERT INTO login(nick, senha) VALUES(?, ?)', (nick, senha))
+        conn.commit()
 
     def update(self, nick, senha):
         novon = str(input('Digite o novo nick: '))
         novas = str(input('Digite a nova senha: '))
 
-        mycursor.execute('UPDATE login SET nick = ?, senha = ? WHERE nick = ? AND senha = ?',
-                         (novon, novas, nick, senha))
-        conect.commit()
+        cur.execute('UPDATE login SET nick = ?, senha = ? WHERE nick = ? AND senha = ?',
+                    (novon, novas, nick, senha))
+        conn.commit()
 
     def delete(self, nick, senha):
-        mycursor.execute('DELETE FROM login WHERE nick = ? AND senha = ?', (nick, senha))
-        conect.commit()
+        cur.execute('DELETE FROM login WHERE nick = ? AND senha = ?', (nick, senha))
+        conn.commit()
 
     def display(self):
-        mycursor.execute('SELECT * FROM login')
+        cur.execute('SELECT * FROM login')
 
         print(f'{"ID":^20}    {"Nick":^20}     {"Senha":^20}')
         print('-' * 50)
 
-        for d in mycursor:
+        for d in cur:
             print(f'{str(d[0]):^20}    {str(d[1]):^20}     {str(d[2]):^20} ')
             print('-' * 25)
             #
